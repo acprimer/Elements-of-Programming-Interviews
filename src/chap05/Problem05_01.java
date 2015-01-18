bef50f7f53b7676443f88ffc9a9ccd863d2a1f42
Dahai Yao <729101421@qq.com>
2015-01-18 10:58:05 +0800

    Problem 5.1

--- src/chap05/Problem05_01.java
+++ src/chap05/Problem05_01.java
@@ -4,4 +4,26 @@ package chap05;
  * Created by yaodh on 2015/1/18.
  */
 public class Problem05_01 {
+    byte parity(int x) {
+        byte ans = 0;
+        while (x != 0) {
+            ans ^= (x & 1);
+            x >>= 1;
+        }
+        return ans;
+    }
+
+    byte parity2(int x) {
+        byte ans = 0;
+        while (x != 0) {
+            ans ^= 1;
+            x &= (x - 1);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        byte ans = new Problem05_01().parity(5);
+        System.out.println(ans);
+    }
 }
